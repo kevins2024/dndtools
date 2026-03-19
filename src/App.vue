0a65de56a7444ac1f62088f3b83b9d427428e4e0
Kevin Smith <kevins2024@outlook.com>
2026-03-19 11:02:52 +0000

    Initial commit, scaffold

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>Empty template</div>
+</template>
+
+<script>
+import dataService from './dataService';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      characters: [],
+      npcs: [],
+      locations: []
+    };
+  },
+  async created() {
+    this.characters = await dataService.get('characters');
+    this.npcs = await dataService.get('npcs');
+    this.locations = await dataService.get('locations');
+  }
+};
+</script>
+
+<style>
+/* Blank white page */
+body, html, #app, div {
+  background: #fff;
+  min-height: 100vh;
+  margin: 0;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: '',
+  productionSourceMap: false,
+  filenameHashing: false,
+};
